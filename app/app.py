61fdb6c0869736142c558eb58627f733a5284f5e
Sebastien Pittet <sebastien@pittet.org>
2025-07-28 22:32:19 +0200

    better typing, reformat code

--- app/app.py
+++ app/app.py
@@ -11,58 +11,57 @@ configuration = exoscalestatus.loadconfig()
 
 # init LAMETRIC logos
 # Country flags and Status logos
-FLAGS = configuration['lametric-country']
-STATUS = configuration['status-logo']
+FLAGS = configuration["lametric-country"]
+STATUS = configuration["status-logo"]
 
 # ############ ROUTES ###############
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "OK"
+    return "Exoscale Status API is running!"
 
 
-@app.route('/api/v1/')
+@app.route("/api/v1/")
 def apiv1():
     # Create the Title frame
     frames = exoscalestatus.init_frames(configuration)
 
     # Request the Exoscale Status page
-    r = exoscalestatus.getExoscaleStatus(
-        configuration['exoscale']['status_url'])
+    r = exoscalestatus.getExoscaleStatus(configuration["exoscale"]["status_url"])
 
     # About the Services
-    services = r['services']
+    services = r["services"]
     services = exoscalestatus.addParentSrv(services)
 
     # About Incidents
-    incidents = r['incidents']
+    incidents = r["incidents"]
 
     if not incidents:
-        frames = exoscalestatus.append_frame(frames, STATUS['up'], "HEALTHY")
+        frames = exoscalestatus.append_frame(frames, STATUS["up"], "HEALTHY")
     else:
         # Add the frames for all the sub-services
-        frames = exoscalestatus.addServiceFrames(
-            services, incidents, frames, STATUS)
+        frames = exoscalestatus.addServiceFrames(services, incidents, frames, STATUS)
 
     # About Maintenances
-    maintenances = r['maintenances']
+    maintenances = r["maintenances"]
     if maintenances:
-        frames = exoscalestatus.append_frame(frames,
-                                             STATUS['tool'],
-                                             "Maintenance scheduled: {}".format(len(maintenances)))
+        frames = exoscalestatus.append_frame(
+            frames,
+            STATUS["tool"],
+            "Maintenance scheduled: {}".format(len(maintenances)),
+        )
     else:
-        frames = exoscalestatus.append_frame(frames, STATUS['tool'],
-                                             "No maintenance.")
+        frames = exoscalestatus.append_frame(frames, STATUS["tool"], "No maintenance.")
 
     return json.dumps(frames)
 
 
 # Testing to check if it works
-@app.route('/test')
+@app.route("/test")
 def test():
     return "OK!"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
